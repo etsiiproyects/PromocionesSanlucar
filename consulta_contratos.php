eb684fa31b5c0191351dee2c9d6bec0de9f52c9f
Fernando <etsiiproyects@gmail.com>
2020-05-26 11:00:49 +0200

    contratos

--- consulta_contratos.php
+++ consulta_contratos.php
@@ -47,7 +47,7 @@
                     <p>Inicio del contrato: <b><?php echo $fila["INICIOALQUILER"]; ?></b></p>
                     <p>Fin del contrato: <b><?php echo $fila["FINALQUILER"]; ?></b></p>
                     <p>Pago mensual: <b><?php echo $fila["PAGOMENSUAL"]; ?></b></p>
-                    <p>NIF cliente: <b><?php echo $fila["NICK"]; ?></b></p>
+                    <p>Nick del cliente: <b><?php echo $fila["EMAIL"]; ?></b></p>
                     <button> Eliminar </button>
                 </div>
             </div>
